a09ba1345ae95c1f775e4c5d0a3cadbbc3b59b81
jae_yoon(mac) <wlwhsvkdlxh@gmail.com>
2023-05-14 19:58:48 +0900

    [자바의 정석] : chap01 자바 인트로

--- Java-JungSeok/src/chap01/Hello.java
+++ Java-JungSeok/src/chap01/Hello.java
@@ -1,7 +1,7 @@
 package chap01;
 
-public class test {
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Hello, world!");
     }
 }
